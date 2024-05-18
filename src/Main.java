b755f574e0ee73662a93dbced87d28456b59f194
UmairSyed1 <blazzinggladiator@hotmail.com>
2024-05-18 17:47:43 +0500

    final update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,37 @@
 
-// This is Main Class
+// This is Main Class....
 public class Main {
     public static void main(String[] args) {
         ToDoList toDoList = new ToDoList();
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("\n1. Add Task \n2. Mark Task as Completed \n3. Display Tasks \n4. Exit");
+        while (true) { 
+            System.out.println("\n(1) Add Task \n(2) Mark Task as Completed \n(3) Display Tasks \n(4) Exit");
             System.out.print("Enter Your Choice: ");
-            int choice = ob.nextInt();
-            scanner.nextLine(); // Consume newline character
+            int choice = ob.nextInt(); 
+            scanner.nextLine(); 
 
             switch (choice) {
                 case 1:
                     System.out.print("Enter task description: ");
                     String description = scanner.nextLine();
-                    toDoList.addTask(description);
-                    break;
+                    toDoList.addTask(description); // To add the task
+                    break; 
                 case 2:
                     System.out.print("Enter task index to mark as completed: ");
                     int index = scanner.nextInt();
-                    toDoList.markTaskAsCompleted(index - 1);
+                    toDoList.markTaskAsCompleted(index - 1); // To mark the completed task
                     break;
                 case 3:
-                    toDoList.displayTasks();
-                    break;
+                    toDoList.displayTasks(); // To display the task 
+                    break; 
                 case 4:
-                    System.out.println("Exiting program.");
-                    System.exit(0);
+                    System.out.println("Exiting program."); // To exit the program
+                    System.exit(0); 
                 default:
-                    System.out.println("Invalid choice. Please try again.");
+                    System.out.println("Invalid choice. Please try again."); // if invalid key is pressed. 
             }
         }
     }
 }
-//                                            THE END
+
