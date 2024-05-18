540ae233779869d2794a69339f04a505189b802f
smartsagittarius2003@gmail.com <smartsagittarius2003@gmail.comgit config --global user.name>
2024-05-18 15:44:05 +0500

    src update

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,25 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
-// this is task class which task we want to do...
- class Task {
-    private String description;
-    private boolean completed;
 
-    public Task(String description) {
-        this.description = description;
-        this.completed = false;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void markAsCompleted() {
-        completed = true;
-    }
-}
-// This is Second class...
-class ToDoList {
-    private List<Task> tasks;
-    public ToDoList() {
-        tasks = new ArrayList<>();
-    }
- // This is Add task method
-    public void addTask(String description) {
-        tasks.add(new Task(description));
-    }
-
-    public void markTaskAsCompleted(int index) {
-        if (index >= 0 && index < tasks.size()) {
-            Task task = tasks.get(index);
-            task.markAsCompleted();
-        } else {
-            System.out.println("Invalid task index.");
-        }
-    }
-// This is display method
-    public void displayTasks() {
-        System.out.println("To-Do List:");
-        for (int i = 0; i < tasks.size(); i++) {
-            Task task = tasks.get(i);
-            System.out.println((i + 1) + ". " + task.getDescription() + " - " + (task.isCompleted() ? "Completed" : "Not Completed"));
-        }
-    }
-}
 // This is Main Class
 public class Main {
     public static void main(String[] args) {
         ToDoList toDoList = new ToDoList();
-        Scanner scanner = new Scanner(System.in);
+        Scanner ob = new Scanner(System.in);
 
         while (true) {
             System.out.println("\n1. Add Task \n2. Mark Task as Completed \n3. Display Tasks \n4. Exit");
             System.out.print("Enter Your Choice: ");
-            int choice = scanner.nextInt();
+            int choice = ob.nextInt();
             scanner.nextLine(); // Consume newline character
 
             switch (choice) {
                 case 1:
                     System.out.print("Enter task description: ");
-                    String description = scanner.nextLine();
+                    String description = ob.nextLine();
                     toDoList.addTask(description);
                     break;
                 case 2:
                     System.out.print("Enter task index to mark as completed: ");
-                    int index = scanner.nextInt();
+                    int index = ob.nextInt();
                     toDoList.markTaskAsCompleted(index - 1);
                     break;
                 case 3:
